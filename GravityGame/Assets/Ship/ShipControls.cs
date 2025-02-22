43c7f606ba5dc7bfe67406e15e842abf26991dce
Juutis <juutis@seri.fi>
2025-02-22 14:05:47 +0200

    movement things

--- GravityGame/Assets/Ship/ShipControls.cs
+++ GravityGame/Assets/Ship/ShipControls.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -18,10 +19,17 @@ public class ShipControls : MonoBehaviour
     private float maxHorizontalRotateSpeed = 180.0f;
     private float minVerticalRotateSpeed = 180.0f;
     private float maxVerticalRotateSpeed = 180.0f;
+    private float xInput = 0.0f;
     private float yInput = 0.0f;
 
     private Rigidbody rb;
 
+    [SerializeField]
+    private CinemachineThirdPersonFollow thirdPersonCamera;
+
+    private float speed;
+    private float maxSpeed = 20.0f;
+    private float minSpeed = 0.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -29,28 +37,30 @@ public class ShipControls : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         forward = transform.rotation;
         Cursor.lockState = CursorLockMode.Locked;
+        zoom = thirdPersonCamera.CameraDistance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.rotation = Quaternion.RotateTowards(transform.rotation, camera.transform.rotation, rotateSpeed * Time.deltaTime);
+        handleZoom();
+        handleAcceleration();
 
         var t = 1.0f;
-        var x = Input.GetAxis("Mouse X");
-        var y = Input.GetAxis("Mouse Y");
+        var x = Input.GetAxisRaw("Mouse X");
+        var y = Input.GetAxisRaw("Mouse Y");
         var z = Input.GetAxis("Horizontal");
-        yInput += y;
-        yInput += -Math.Sign(yInput) * 10f * Time.deltaTime;
         //if(!WorldManager.main.invertControls) y = -y;
+        
+        xInput += x;
+        yInput += y;
 
         var horizontalRotateSpeed = Mathf.Lerp(minHorizontalRotateSpeed, maxHorizontalRotateSpeed, t);
         var verticalRotateSpeed = Mathf.Lerp(minVerticalRotateSpeed, maxVerticalRotateSpeed, t);
-        var yawRotation = shipMesh.transform.right.y * Mathf.Lerp(10.0f, 25.0f, (1-t));
 
         var roll = z * Time.deltaTime * verticalRotateSpeed;
         var pitch = -0.01f * yInput * Time.deltaTime * verticalRotateSpeed;
-        var yaw = -x * Time.deltaTime * horizontalRotateSpeed;
+        var yaw = -0.01f * xInput * Time.deltaTime * horizontalRotateSpeed;
 
         forward = forward * Quaternion.AngleAxis(roll, transform.forward);
         forward = forward * Quaternion.AngleAxis(pitch, transform.right);
@@ -59,10 +69,41 @@ public class ShipControls : MonoBehaviour
         shipMesh.transform.Rotate(-Vector3.forward, roll);
         shipMesh.transform.Rotate(Vector3.right, pitch);
         shipMesh.transform.Rotate(-Vector3.up, yaw);
+        
+        xInput = Mathf.MoveTowards(xInput, 0.0f, 40f * Time.deltaTime);
+        yInput = Mathf.MoveTowards(yInput, 0.0f, 20f * Time.deltaTime);
     }
 
     void FixedUpdate()
     {
-        rb.linearVelocity = shipMesh.transform.forward * 10.0f;
+        rb.linearVelocity = shipMesh.transform.forward * speed;
+        Debug.Log(rb.linearVelocity);
+    }
+
+    private float minZoom = 2.0f;
+    private float maxZoom = 15.0f;
+    private float zoom;
+    private float zoomDiff;
+
+    void handleZoom() {
+        zoomDiff += Input.GetAxis("Mouse ScrollWheel") * 5;
+        zoom += zoomDiff * Time.deltaTime;
+        if (zoom > maxZoom) {
+            zoom = maxZoom;
+            zoomDiff = 0.0f;
+        }
+        if (zoom < minZoom) {
+            zoom = minZoom;
+            zoomDiff = 0.0f;
+        }
+        zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+        thirdPersonCamera.CameraDistance = zoom;
+        zoomDiff = Mathf.MoveTowards(zoomDiff, 0.0f, 10f * Time.deltaTime);
+    }
+
+    void handleAcceleration() {
+        speed += Input.GetAxis("Vertical") * Time.deltaTime * 10.0f;
+        speed = Mathf.Clamp(speed, minSpeed, maxSpeed);
+        Debug.Log(speed);
     }
 }
