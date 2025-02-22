ebbb0f2a04f7cedbf913a6ee8e3e4916dc819611
Juutis <juutis@seri.fi>
2025-02-22 20:15:42 +0200

    Added spawner for gobbos

---
+++ GravityGame/Assets/Ship/ShipSpawner.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class ShipSpawner : MonoBehaviour
+{
+    private GobboShip shipAi;
+    private float spawned;
+    private float spawnDuration = 2.0f;
+    private bool started = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        shipAi = GetComponentInChildren<GobboShip>(true);
+        shipAi.enabled = false;
+        spawned = Time.time;
+        shipAi.gameObject.SetActive(false);
+        Invoke("go", Random.Range(0.0f, 0.5f));
+    }
+
+    public void go() {
+        started = true;
+        shipAi.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!started) {
+            return;
+        }
+        var t = (Time.time - spawned) / spawnDuration;
+        Mathf.Clamp(t, 0.0f, 1.0f);
+        var scaleX = Mathf.Lerp(0.0f, 1.0f, t);
+        var scaleY = Mathf.Lerp(0.0f, 1.0f, t);
+        var scaleZ = Mathf.Lerp(100.0f, 1.0f, t);
+        transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
+        if (t >= 0.999) {
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            unleashGobbo();
+        }
+    }
+
+    void unleashGobbo() {
+        shipAi.enabled = true;
+        shipAi.transform.parent = null;
+        Destroy(gameObject);
+    }
+}
