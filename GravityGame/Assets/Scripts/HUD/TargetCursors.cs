52daa93d873ab8eed2e100a755326763910519ca
Matias Pitkänen <M2tias@Gmail.com>
2025-02-23 15:19:35 +0200

    Refactoring hud cursors

--- GravityGame/Assets/Scripts/HUD/TargetCursors.cs
+++ GravityGame/Assets/Scripts/HUD/TargetCursors.cs
@@ -7,18 +7,21 @@ using UnityEngine.UI;
 public class TargetCursors : MonoBehaviour
 {
     [SerializeField]
-    private List<TargetCursor> targetCursors = new();
+    private List<TargetCursorConfig> targetCursorConfig = new();
     [SerializeField]
     private Camera cam;
     [SerializeField]
     private Canvas canvas;
     [SerializeField]
     private float borderPad;
+    [SerializeField]
+    private ShipControls player;
 
     private float pollTime = 1f;
     private float lastPoll = 0f;
 
     private Dictionary<string, GameObject> cursorCache = new();
+    private Dictionary<string, List<CursorInstance>> cursorInstances = new();
 
 
     void Start()
@@ -31,64 +34,151 @@ public class TargetCursors : MonoBehaviour
     {
         if (Time.time - lastPoll > pollTime)
         {
-            lastPoll = Time.time;
-            foreach (TargetCursor targetCursor in targetCursors)
-            {
-                List<GameObject> objs = GameObject.FindGameObjectsWithTag(targetCursor.tag).ToList();
-                GameObject nearest = objs.OrderBy(x => Vector3.Distance(x.transform.position, transform.position)).FirstOrDefault();
-                AddOrUpdate(cursorCache, targetCursor.tag, nearest);
-            }
+            Debug.Log($"PROBING: {Time.time}");
+            ProbeTargets();
         }
 
-        foreach (KeyValuePair<string, GameObject> entry in cursorCache)
+        UpdateCursors();
+    }
+
+    private void UpdateCursors()
+    {
+        foreach (KeyValuePair<string, List<CursorInstance>> entry in cursorInstances)
         {
-            RectTransform rectTransform = canvas.GetComponent<RectTransform>();
-            Vector2 center = rectTransform.rect.center;
-            Vector2 size = rectTransform.rect.size;
-            Vector3 vpPos = cam.ViewportToScreenPoint(cam.WorldToViewportPoint(entry.Value.transform.position));
-            float yMin = center.y + borderPad;
-            float yMax = center.y + size.y - borderPad;
-            float xMin = center.x + borderPad;
-            float xMax = center.x + size.x - borderPad;
-
-            float k1 = size.y / size.x;
-            float k2 = -size.y / size.x;
-
-            // if (vpPos.z < 0)
-            // {
-            //     if (vpPos.y > k1 * vpPos.x && vpPos.y > k2 * vpPos.x)
-            //     {
-            //         vpPos.y = yMax;
-            //     }
-            //     else if (vpPos.y < k1 * vpPos.x && vpPos.y < k2 * vpPos.x)
-            //     {
-            //         vpPos.y = yMin;
-            //     }
-            //     else if (vpPos.y < k1 * vpPos.x && vpPos.y > k2 * vpPos.x)
-            //     {
-            //         vpPos.x = xMax;
-            //     }
-            //     else if (vpPos.y > k1 * vpPos.x && vpPos.y < k2 * vpPos.x)
-            //     {
-            //         vpPos.x = xMin;
-            //     }
-            // }
-
-            Vector3 clamped = new Vector3(Mathf.Clamp(vpPos.x, xMin, xMax), Mathf.Clamp(vpPos.y, yMin, yMax), vpPos.z);
-
-            if (vpPos.z < 0 && entry.Key == "Respawn")
+            foreach (CursorInstance instance in entry.Value)
             {
+                RectTransform rectTransform = canvas.GetComponent<RectTransform>();
+                Vector2 center = rectTransform.rect.center;
+                Vector2 size = rectTransform.rect.size;
+                Vector3 vpPos = cam.ViewportToScreenPoint(cam.WorldToViewportPoint(instance.cursor.transform.position));
+                float yMin = center.y + borderPad;
+                float yMax = center.y + size.y - borderPad;
+                float xMin = center.x + borderPad;
+                float xMax = center.x + size.x - borderPad;
+
+                float k1 = size.y / size.x;
+                float k2 = -size.y / size.x;
+
+                // if (vpPos.z < 0)
+                // {
+                //     if (vpPos.y > k1 * vpPos.x && vpPos.y > k2 * vpPos.x)
+                //     {
+                //         vpPos.y = yMax;
+                //     }
+                //     else if (vpPos.y < k1 * vpPos.x && vpPos.y < k2 * vpPos.x)
+                //     {
+                //         vpPos.y = yMin;
+                //     }
+                //     else if (vpPos.y < k1 * vpPos.x && vpPos.y > k2 * vpPos.x)
+                //     {
+                //         vpPos.x = xMax;
+                //     }
+                //     else if (vpPos.y > k1 * vpPos.x && vpPos.y < k2 * vpPos.x)
+                //     {
+                //         vpPos.x = xMin;
+                //     }
+                // }
+
+                Vector3 clamped = new Vector3(Mathf.Clamp(vpPos.x, xMin, xMax), Mathf.Clamp(vpPos.y, yMin, yMax), vpPos.z);
+
+                if (vpPos.z < 0 && entry.Key == "Respawn")
+                {
+                }
+                else if (entry.Key == "Respawn")
+                {
+
+                }
+
+                TargetCursorConfig cursor = targetCursorConfig.FirstOrDefault(x => x.tag == entry.Key);
+                instance.cursor.GetComponent<RectTransform>().rotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * Mathf.Atan2(vpPos.y, vpPos.x) - 90);
+
+                instance.cursor.GetComponent<RectTransform>().position = clamped;
             }
-            else if (entry.Key == "Respawn")
-            {
-
-            }
-
-            TargetCursor cursor = targetCursors.FirstOrDefault(x => x.tag == entry.Key);
-            cursor.img.GetComponent<RectTransform>().rotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * Mathf.Atan2(vpPos.y, vpPos.x) - 90);
-
-            cursor.img.GetComponent<RectTransform>().position = clamped;
         }
+    }
+
+    private void ProbeTargets()
+    {
+        lastPoll = Time.time;
+        // foreach (TargetCursorConfig targetCursor in targetCursorConfig)
+        // {
+        //     List<GameObject> objs = GameObject.FindGameObjectsWithTag(targetCursor.tag).ToList();
+        //     GameObject nearest = objs.OrderBy(x => Vector3.Distance(x.transform.position, transform.position)).FirstOrDefault();
+        //     AddOrUpdate(cursorCache, targetCursor.tag, nearest);
+        // }
+
+        foreach (TargetCursorConfig targetCursor in targetCursorConfig)
+        {
+            string curTag = targetCursor.tag;
+
+            IEnumerable<GameObject> targets = GameObject.FindGameObjectsWithTag(curTag);
+
+            if (!targets.Any(x => x != null))
+            {
+                continue;
+            }
+
+            if (targetCursor.trackInRange)
+            {
+                targets = targets.Where(t => Vector3.Distance(player.transform.position, t.transform.position) < targetCursor.trackRange);
+
+                if (!targets.Any(x => x != null))
+                {
+                    continue;
+                }
+            }
+
+
+            if (!targetCursor.trackAll)
+            {
+                if (cursorInstances.ContainsKey(curTag))
+                {
+                    GameObject target = targets.FirstOrDefault();
+
+                    if (cursorInstances[curTag].Any(x => x.target == target))
+                    {
+                        continue;
+                    }
+
+                    CursorInstance instance = InstantiateCursor(targetCursor, targets);
+                    cursorInstances[curTag].Add(instance);
+                }
+                else
+                {
+                    CursorInstance instance = InstantiateCursor(targetCursor, targets);
+
+                    cursorInstances.Add(curTag, new() { instance });
+                }
+            }
+            else
+            {
+                if (!cursorInstances.ContainsKey(curTag))
+                {
+                    cursorInstances.Add(curTag, new() { });
+                }
+
+                foreach (var target in targets)
+                {
+                    if (cursorInstances[curTag].Any(x => x.target == target))
+                    {
+                        continue;
+                    }
+
+                    CursorInstance instance = InstantiateCursor(targetCursor, targets);
+                    cursorInstances[curTag].Add(instance);
+                }
+            }
+        }
+    }
+
+    private CursorInstance InstantiateCursor(TargetCursorConfig cursorConfig, IEnumerable<GameObject> targets)
+    {
+        CursorInstance instance = new();
+        instance.cursor = Instantiate(cursorConfig.cursorPrefab);
+        instance.target = targets.FirstOrDefault();
+        instance.config = cursorConfig;
+        instance.cursor.transform.parent = transform;
+        return instance;
     }
 
     private void AddOrUpdate(Dictionary<string, GameObject> dict, string key, GameObject value)
@@ -105,8 +195,18 @@ public class TargetCursors : MonoBehaviour
 }
 
 [Serializable]
-public class TargetCursor
+public class TargetCursorConfig
 {
     public string tag;
-    public Image img;
+    public GameObject cursorPrefab;
+    public bool trackAll;
+    public bool trackInRange;
+    public float trackRange;
+}
+
+public class CursorInstance
+{
+    public GameObject cursor;
+    public GameObject target;
+    public TargetCursorConfig config;
 }
