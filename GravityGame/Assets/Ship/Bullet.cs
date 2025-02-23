144ac1d538aa8eae647a5966fb7bd67f991eb479
Juutis <juutis@seri.fi>
2025-02-23 14:52:27 +0200

    Added main scene

--- GravityGame/Assets/Ship/Bullet.cs
+++ GravityGame/Assets/Ship/Bullet.cs
@@ -32,8 +32,18 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Physics.Raycast(transform.position, transform.forward, rb.linearVelocity.magnitude * Time.deltaTime, layerMask)) {
+        RaycastHit hitInfo;
+        var t = rb.linearVelocity.magnitude * Time.deltaTime;
+        var dir = rb.linearVelocity.normalized;
+        if (Physics.Raycast(transform.position - dir * t, dir, out hitInfo, t * 3, layerMask)) {
+            transform.position = hitInfo.point;
+            rb.position = hitInfo.point;
             Kill();
         }
+    }
+
+    void FixedUpdate()
+    {
+
     }
 }
